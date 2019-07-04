822dfde31e4f32ccabf18747e4ab463c274b2440
alexander.londono <alexander.londono@grupokonecta.com>
2019-07-04 15:26:29 -0500

    New users

--- app.js
+++ app.js
@@ -7,7 +7,7 @@ const session = require('express-session');
 const hbs = require('hbs');
 const bodyParser = require('body-parser');
 
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 9000;
 
 var app = express();
 
